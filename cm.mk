e91f888bc94ee29584a9a2174c7b35416bd2b9ed
topet2k12001 <topet2k12001@users.noreply.github.com>
2016-08-14 00:55:50 +0800

    Update cm.mk
    Line 1: changed to d858hk
    Line 9: changed to cm_d858hk
    Line: 13: changed to g3_open_hk
    Line: 14: changed to g3_open_hk
    Line: 15: changed to g3_open_hk-user

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/lge/d856/full_d856.mk)
+$(call inherit-product, device/lge/d858hk/full_d858hk.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
@@ -6,10 +6,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
-PRODUCT_NAME := cm_d856
+PRODUCT_NAME := cm_d858hk
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
-    PRODUCT_NAME="g3_open_cis" \
-    BUILD_FINGERPRINT="lge/g3_open_cis/g3:$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):user/release-keys" \
-    PRIVATE_BUILD_DESC="g3_open_cis-user $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) release-keys"
+    PRODUCT_NAME="g3_open_hk" \
+    BUILD_FINGERPRINT="lge/g3_open_hk/g3:$(PLATFORM_VERSION)/$(BUILD_ID)/$(BUILD_NUMBER):user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_open_hk-user $(PLATFORM_VERSION) $(BUILD_ID) $(BUILD_NUMBER) release-keys"
